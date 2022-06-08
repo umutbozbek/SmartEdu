76de6abba3d8888696035cdb47c2b4663a682120
umutbozbek <umutbozbek58@gmail.com>
2022-06-08 17:34:19 +0300

    Linkleri Güncellemek

--- app.js
+++ app.js
@@ -3,9 +3,27 @@ const express=require('express')
 
 const app=express();
 
+//Tamplate Engine
+app.set("view engine", "ejs")
+
+
+// MiddleWares
+app.use(express.static("public"))
+
+//Routes
 app.get('/',(req,res)=>{
-    res.status(200).send('INDEX SAYFASI')
-})
+    res.status(200).render('index',{
+        page_name: "index"
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.status(200).render('about',{
+        page_name: "about"
+    });
+});
+
+
 
 
 const port=3000;
