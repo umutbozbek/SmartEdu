7343109395b689b2fa5c11e05e6842ca3cfdb61f
umutbozbek <umutbozbek58@gmail.com>
2022-06-08 17:54:43 +0300

    Routing

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express=require('express')
-
+const pageRoute=require('./routes/pageRoute')
 
 const app=express();
 
@@ -11,17 +11,9 @@ app.set("view engine", "ejs")
 app.use(express.static("public"))
 
 //Routes
-app.get('/',(req,res)=>{
-    res.status(200).render('index',{
-        page_name: "index"
-    });
-});
+app.use('/',pageRoute);
 
-app.get('/about',(req,res)=>{
-    res.status(200).render('about',{
-        page_name: "about"
-    });
-});
+
 
 
 
