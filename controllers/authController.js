328c1f4e1887079d2770191a54ff539a30050e5f
umutbozbek <umutbozbek58@gmail.com>
2022-06-11 17:59:47 +0300

    Kayıt Sayfası ve Bcrypt

---
+++ controllers/authController.js
@@ -0,0 +1,19 @@
+const User = require('../models/User')
+
+exports.createUser = async (req, res) => {
+
+    try {
+        const user = await User.create(req.body)
+        res.status(201).json({
+            status: 'succes',
+            user
+        })
+        
+    } catch (error) {
+        res.status(400).json({
+            status: 'failed',
+            error
+
+        })
+    }
+}
---
+++ models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const bcrypt=require('bcrypt')
+const Scheema = mongoose.Schema
+
+
+const UserSchema = new Scheema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+
+});
+
+
+UserSchema.pre('save',function(next){
+    const user=this;
+    bcrypt.hash(user.password,10,(error,hash)=>{
+        user.password=hash
+        next()
+    })
+})
+
+
+
+const User = mongoose.model('User', UserSchema)
+
+module.exports = User     
