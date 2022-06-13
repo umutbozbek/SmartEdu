0ed73b0528f44c0f8b827353d9fc829ca3fee94f
umutbozbek <umutbozbek58@gmail.com>
2022-06-13 20:12:00 +0300

    Kişiye özel içerik

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ exports.loginUser = async (req, res) => {
             if (same) {
               // USER SESSION
               req.session.userID=user._id
-              res.status(200).redirect('/')
+              res.status(200).redirect('/users/dashboard')
             }
           });
         }
@@ -48,3 +48,12 @@ exports.loginUser = async (req, res) => {
       res.redirect('/')
     })
   } 
+
+
+  exports.getDashboardPage =async (req, res) => {
+    const user=await User.findOne({_id:req.session.userID})
+    res.status(200).render('dashboard', {
+        page_name: "dashboard",
+        user
+    });
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -8,6 +8,7 @@ const router=express.Router();
 router.route('/signup').post(authController.createUser) //http://localhost:3000/user/signup
 router.route('/login').post(authController.loginUser)
 router.route('/logout').get(authController.logoutUser)
+router.route('/dashboard').get(authController.getDashboardPage)//http://localhost:3000/user/dashboard
 
 
 
