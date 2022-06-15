a69ed2acff55c844d0a6087a2b65317ed49207ae
umutbozbek <umutbozbek58@gmail.com>
2022-06-15 23:09:29 +0300

    İletişim Sayfası ve Mail Göndermek

--- controllers/pageContoller.js
+++ controllers/pageContoller.js
@@ -1,4 +1,4 @@
-
+const nodemailer = require("nodemailer");
 
 exports.getIndexPage = ((req, res) => {
     console.log(req.session.userID)
@@ -26,3 +26,51 @@ exports.getLoginPage = ((req, res) => {
         page_name: "login"
     });
 })
+
+exports.getContactPage = ((req, res) => {
+    res.status(200).render('contact', {
+        page_name: "contact"
+    });
+})
+
+exports.sendEmail = async (req, res) => {
+
+    const outputMassage = `
+
+<h1> Mail Details </h1>
+<ul>
+    <li> Name : ${req.body.name} </li>
+    <li> Email : ${req.body.email} </li>
+</ul>
+<h1> Massage  </h1>
+    <p> ${req.body.message}</p>
+    `
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "umutbozbek58@gmail.com", // gmail accaount
+          pass: "gjsjzkqjegtmecry",//gmail password
+        },
+      });
+    
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"Smart EDU Contact From" <umutbozbek58@gmail.com>', // sender address
+        to: "at.bozbek@gmail.com", // list of receivers
+        subject: "Smart EDU Contact From Massage ", // Subject line
+        html: outputMassage, // html body
+      });
+    
+      console.log("Message sent: %s", info.messageId);
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    
+      // Preview only available when sending through an Ethereal account
+      console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    
+      res.status(200).redirect('/contact')
+
+}
