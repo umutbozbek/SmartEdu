4c9b3d8066bfd6d878cda09aa0710cd89886dbeb
umutbozbek <umutbozbek58@gmail.com>
2022-06-17 13:39:57 +0300

    Kurs Güncelleme

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -8,6 +8,7 @@ router.route('/').post(roleMiddleware(["teacher","admin"]),courseController.crea
 router.route('/').get(courseController.getAllCourses)  //http://localhost:3000/courses
 router.route('/:slug').get(courseController.getCourse)
 router.route('/:slug').delete(courseController.deleteCourse)
+router.route('/:slug').put(courseController.updateCourse)
 router.route('/enroll').post(courseController.enrollCourse)
 router.route('/release').post(courseController.releaseCourse)
 
